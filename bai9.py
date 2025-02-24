7ddbb7e567dbc2900c5a3d18a63458f34645571a
Vũ Đức Quang Huy <quanghuy25062005@gmail.com>
2025-02-25 01:23:00 +0700

    bài 9 python basic

---
+++ bai9.py
@@ -0,0 +1,47 @@
+#arithmetic operators
+x=20
+y=10
+print(x+y)
+print(x-y)
+print(x*y)
+print(x%y)
+print(x/y)
+print(x**y)
+print(x//y)
+print()
+#assignment operators
+def assignment_operators():
+    x1=5
+    print(x1)
+    x1+=3
+    print(x1)
+    x1-=5
+    print(x1)
+    x1*=5
+    print(x1)
+    x1%=6
+    print(x1)
+    x1/=6
+    print(x1)
+    x1**=2
+    print(x1)
+    x1//=9
+    print(x1)   
+assignment_operators()
+#comparison operators
+x2=5
+x3=1
+print(x2==x3)
+print(x2<x3)
+print(x2>x3)
+print(x2!=x3)
+#logical operators
+y1=1
+y2=2
+if(y1<3 and y2<3):
+    print("okokokok")
+if(y1>1 or y2 >1):
+    print("okokok")
+if(not(y1<3 and y2<2)):
+    print("abc")
+
