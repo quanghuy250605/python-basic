c88c722ae6c67eb6bf4493432be8e6c7c5db5a15
Vũ Đức Quang Huy <quanghuy25062005@gmail.com>
2025-03-01 18:57:08 +0700

    bài 19 python basic

---
+++ bai19.py
@@ -0,0 +1,14 @@
+fruits =["apple","banana","orange"]
+print(fruits[0])
+fruits[0]="mango"
+print(fruits)
+print(len(fruits))
+#loop
+for i in fruits:
+    print(i)
+#adding
+fruits.append("apple")
+print(fruits)
+#remove
+fruits.remove("apple")
+print(fruits)
