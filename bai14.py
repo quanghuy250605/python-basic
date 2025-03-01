9392d5a34f1d9d1f062244e060cc3ae51d71e818
Vũ Đức Quang Huy <quanghuy25062005@gmail.com>
2025-03-01 16:59:55 +0700

    bài 14 python basic

---
+++ bai14.py
@@ -0,0 +1,30 @@
+x=20
+y=25
+if x<y:
+    print("20<25")
+#dk trên sai thì thử đk dưới elif
+a =20
+b= 20
+if a>b :
+    print("a>b")
+elif a == b:
+    print("a=b")
+#else
+if a>b:
+    print("a>b")
+elif a<b :
+    print("a<b")
+else : print("a = b")  
+#and
+if a==b and a>10 :
+    print("cả 2 đk đều đúng")
+#or
+if a>b or a > 10:
+    print("ít nhất có 1 đk đúng")
+#not
+if not a>b :
+    print("a = b")
+if a>10:
+    print("a>10")
+    if a>15 :
+        print("a>15")
