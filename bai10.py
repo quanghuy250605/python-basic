de15321c7ae44fb67e66a723c9b8f3527b6414d8
Vũ Đức Quang Huy <quanghuy25062005@gmail.com>
2025-03-01 16:32:51 +0700

    bài 10 python basic

--- bai10.py
+++ bai10.py
@@ -28,3 +28,21 @@ print(x)
 #remove item 
 x.remove("aaa")
 print(x)
+#remove entire list
+#del x
+#loop
+for a in x:
+    print(a, end = " ")
+print()
+#count
+for i in range(len(x)):
+    print(i, end=" ")
+print()
+newlist= [u for u in x if "h" in u]
+print(newlist)
+#sapxep alphabet
+x.sort()
+print(x)
+x1=[4,5,6,3,6,7,8,35,66]
+x1.sort()
+print(x1)
