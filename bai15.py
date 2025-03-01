73ba0ae7678bac62d5510688d49c72224cdc442f
Vũ Đức Quang Huy <quanghuy25062005@gmail.com>
2025-03-01 17:43:18 +0700

    bài 15 python basic

---
+++ bai15.py
@@ -0,0 +1,22 @@
+i=1
+while i<10:
+    print(i,end = " ")
+    i+=1
+print()
+#break
+a=1
+while a<8:
+    print(a, end = " ")
+    if a==5:
+        break
+    a+=1
+print()
+#continue
+i=1
+while i<6:
+    i+=1
+    if i == 3:
+        continue
+    print(i)
+
+   
